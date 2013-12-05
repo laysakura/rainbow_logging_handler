cf8c032ce24cf9ec97ecbee07a780aa6b1b75d8d
Sho Nakatani <lay.sakura@gmail.com>
2013-12-05 09:49:11 +0900

    more readable test

--- rainbow_logging_handler/test/test_logging.py
+++ rainbow_logging_handler/test/test_logging.py
@@ -5,15 +5,15 @@ import logging
 from rainbow_logging_handler import RainbowLoggingHandler
 
 
-def test_usage():
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
-
+def setup():
+    logger = logging.getLogger('test_logging')
+    logger.setLevel(logging.DEBUG)
     handler = RainbowLoggingHandler(sys.stderr)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    handler.setFormatter(formatter)
-    root_logger.addHandler(handler)
-    logger = logging.getLogger('test')
+    logger.addHandler(handler)
+
+
+def test_usage():
+    logger = logging.getLogger('test_logging')
 
     logger.debug("debug msg")
     logger.info("info msg")
