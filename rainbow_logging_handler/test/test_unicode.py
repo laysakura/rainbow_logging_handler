41e5d1e8acafcfc723b1e7b7eb171bfcaf598346
Sho Nakatani <lay.sakura@gmail.com>
2013-12-05 14:57:55 +0900

    adds test_unicode.py

---
+++ rainbow_logging_handler/test/test_unicode.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from nose.tools import *
+import sys
+import logging
+from rainbow_logging_handler import RainbowLoggingHandler
+
+
+def setup():
+    logger = logging.getLogger('test_unicode')
+    logger.setLevel(logging.DEBUG)
+    handler = RainbowLoggingHandler(sys.stderr)
+    logger.addHandler(handler)
+
+
+def test_unicode():
+    logger = logging.getLogger('test_unicode')
+    logger.debug('日本語ログ')
+    logger.debug(u'日本語ログ')
